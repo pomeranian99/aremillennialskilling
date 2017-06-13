3b9fe7a5b653ffb9a042ff58ed5ebcb35c8cca4f
Clive Thompson <clive@clivethompson.net>
2017-06-13 13:15:24 -0400

    Create millennial_search.js

--- millennial_search.js
+++ millennial_search.js
@@ -1,4 +1,4 @@
-var thoseMeddlingKids = "why millennials are killing "
+var thoseMeddlingKids = "why millennials are killing -buffalo "
 
 function Gsitesearch(curobj){
 curobj.q.value = thoseMeddlingKids+curobj.qfront.value
